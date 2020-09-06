efc1886d1bf1de3282eedc0c359234c1479aa590
Karran Lemos <karranlemos@gmail.com>
2020-09-06 10:10:20 -0300

    Change name to GitHub Lister and remove favicon

--- src/components/layout/inc/Navbar.js
+++ src/components/layout/inc/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav id="nav">
             <div className="container">
-                <Link className="nav-button main-button" to="/">Github Checker</Link>
+                <Link className="nav-button main-button" to="/">GitHub Lister</Link>
                 <div className="nav-options">
                     <Link className="nav-button" to="/">Home</Link>
                     <Link className="nav-button" to="/about">About</Link>
